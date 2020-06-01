b015bfe044f38f57470c11ed18fe616f254acd34
zhijiaju <952407502@qq.com>
2020-06-01 13:51:31 +0800

    --监听器的修改

--- src/main/java/com/hash/excel/listiner/ExcelListener.java
+++ src/main/java/com/hash/excel/listiner/ExcelListener.java
@@ -11,7 +11,7 @@ import java.util.Map;
 //创建读取excel监听器
 public class ExcelListener extends AnalysisEventListener<ReadData> {
 
-    //创建list集合封装最终的数据
+    //创建list集合封装最终的数据  批量存入
     List<ReadData> list = new ArrayList<ReadData>();
 
     //一行一行去读取excle内容
@@ -19,6 +19,7 @@ public class ExcelListener extends AnalysisEventListener<ReadData> {
     public void invoke(ReadData user, AnalysisContext analysisContext) {
         System.out.println("***"+user);
         list.add(user);
+        //批量插入数据
 
     }
 
@@ -27,9 +28,10 @@ public class ExcelListener extends AnalysisEventListener<ReadData> {
         System.out.println("表头信息："+headMap);
     }
 
-    //读取完成后执行
+    //读取完所有的数据后执行
     @Override
     public void doAfterAllAnalysed(AnalysisContext analysisContext) {
+        //存取遗留的数据到数据库中
 
     }
 }
